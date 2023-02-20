f175d466e6efcded44474b9ea7ca51e5b130eab0
Karen <karen.czock@bsbkoeln.de>
2023-02-20 16:19:12 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -58,22 +58,22 @@
 <style lang="scss">
 
   .filter-outline {
-    filter: url(https://czkaa.github.io/insert-title-here/#outline);
+    filter: url(http://czkaa.github.io/insert-title-here/#outline);
   }
 
   .filter-outline-xs {
-    filter: url(https://czkaa.github.io/insert-title-here/#outline-xs);
+    filter: url(http://czkaa.github.io/insert-title-here/#outline-xs);
   }
 
   .filter-blur {
-    filter: url(https://czkaa.github.io/insert-title-here/#blur);
+    filter: url(http://czkaa.github.io/insert-title-here/#blur);
     &:hover {
-      filter: url(https://czkaa.github.io/insert-title-here/#blur-hover);
+      filter: url(http://czkaa.github.io/insert-title-here/#blur-hover);
     }
   }
 
   .filter-blur2 {
-    filter: url(https://czkaa.github.io/insert-title-here/#blur2);
+    filter: url(http://czkaa.github.io/insert-title-here/#blur2);
   }
 
   .wireframe-border {
@@ -92,7 +92,7 @@
 
    .wireframe-text-sm {
     @apply text-white;
-    filter: url(https://http://czkaa.github.io/insert-title-here/#outline);
+    filter: url(http://czkaa.github.io/insert-title-here/#outline);
   }
 
 
