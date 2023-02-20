cdd58b0f66155cc1c504fba340c437bd9f87d8e6
Karen <karen.czock@bsbkoeln.de>
2023-02-20 15:16:54 +0100

    update

--- src/components/template/TemplateHeader.vue
+++ src/components/template/TemplateHeader.vue
@@ -2,7 +2,10 @@
     <header layout-header class="bg-white border border-secondary wireframe-background rounded-full hover:bg-tertiary w-11/12 h-30 hover:w-full cursor-pointer transition-all duration-1000 flex flex-col justify-center items-center overflow-hidden flex-shrink-0">
       <h1 class="text-xl font-sans p-2 my-2 ">
         <div class="border border-dashed border-white ">
-          <BasicInput style="letter-spacing: -0.05em;" placeholder="Insert Title Here" text="Dysfunctional Templates" class="whitespace-nowrap wireframe-text-sm text-white block w-full mt-24 overflow-hidden leading-none outline-none bg-transparent text-center placeholder-white" />
+          <BasicInput style="letter-spacing: -0.05em;" 
+            placeholder="1Insert Title Here" 
+            text="Dysfunctional Templates" 
+            class="whitespace-nowrap wireframe-text-sm text-white block w-full mt-24 overflow-hidden leading-none outline-none bg-transparent text-center placeholder-white" />
         </div>
       </h1>
     </header>
